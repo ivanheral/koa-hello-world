7126fdb75b0ceed9a84d1da393d3629de07fb0b0
ivanheral <ivanhernandezal@gmail.com>
2020-09-30 19:26:12 +0200

    test express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const Koa = require('koa');
 const app = new Koa();
 
@@ -6,3 +7,16 @@ app.use(async ctx => {
 });
 
 app.listen(3000);
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
