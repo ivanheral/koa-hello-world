534a3d1e4a5bf2a80496ed2e65b3d95eaefe5524
ivanheral <ivanhernandezal@gmail.com>
2020-09-30 20:19:18 +0200

    express

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-import { serve } from "https://deno.land/std@0.59.0/http/server.ts";
-import { parse } from "https://deno.land/std/flags/mod.ts";
+const express = require('express')
+const app = express()
+const port = 3000
 
-const { args } = Deno;
-const PUERTO_DEFAULT = 8000;
-const puerto = parse(args).port;
-const server = serve({ port: puerto ? Number(puerto) : PUERTO_DEFAULT });
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-for await (const peticion of server ) {
-  const colorRandom = Math.floor(Math.random() * 16777215).toString(16);
-  const contenidos = `<h1 style="color:#${colorRandom}">HOLA DENO!</h1>`;
-
-  peticion.respond({ body: contenidos });
-}
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
