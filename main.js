735ecfad7b91407897bc40399ee70b458e2f4114
ivanheral <ivanhernandezal@gmail.com>
2020-10-01 08:06:36 +0200

    update

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-const express = require('express')
-const app = express()
 const port = process.env.PORT || 5000
+const Koa = require('koa');
+const app = new Koa();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
 
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port);
